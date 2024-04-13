daed47a51ffc51dae0e7f510111fc59ffb6ddd41
Zoe <zoe.bell@uvm.edu>
2024-04-13 12:03:29 -0400

    user input validation for driver

--- Driver.java
+++ Driver.java
@@ -6,42 +6,47 @@ public class Driver{
     public static void main(String [] args){
         learnPython();
     }
-    public static void learnPython(){
+    public static void learnPython() {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Welcome to the class! We will be learning Python today");
-        boolean exit = false;
-        while(!exit) {
-            System.out.println("Let's begin! Enter (e) at any point to exit.");
-            System.out.println("Choose a module to start learning: ");
-            System.out.println("(1) for Module 1");
-            System.out.println("(2) for Module 2");
-            System.out.println("(3) for Module 3");
-            System.out.println("(4) for Module 4");
-            // Get user choice for the module
-            String choice = keyboard.nextLine();
-            try {
-                int newChoice = Integer.parseInt(choice);
+        System.out.println("Welcome to the class! We will be learning Python today.");
+        boolean active = true;
+        while (active) {
+            boolean validInput = false;
+            String choice = "";
+            while (!validInput) {
+                System.out.println("Enter (e) at any point to exit.");
+                System.out.println("Choose a module to start learning: ");
+                System.out.println("(1) for Module 1");
+                System.out.println("(2) for Module 2");
+                System.out.println("(3) for Module 3");
+                System.out.println("(4) for Module 4");
+                // Get user choice for the module
+                choice = keyboard.nextLine();
+                try {
+                    int newChoice = Integer.parseInt(choice);
+                    if (newChoice == 1 || newChoice == 2 || newChoice == 3 || newChoice == 4) {
 
-                if (newChoice == 1) {
-
-                } else if (newChoice == 2) {
-
-                } else if (newChoice == 3) {
-
-                } else if (newChoice == 4){
-
-                } else {
+                        validInput = true;
+                    } else {
+                        System.out.println("Invalid");
+                    }
+                } catch (NumberFormatException e) {
+                    if (choice == "e") {
+                        active = false;
+                        validInput = true;
+                    }
                     System.out.println("Enter 1, 2, 3, or 4 to choose a Module or enter (e) to exit: ");
                     choice = keyboard.nextLine();
                 }
-
-            }
-            catch (NumberFormatException e) {
-                if (choice == "e") {
-                    System.exit(0);
-                }
-                System.out.println("Enter 1, 2, 3, or 4 to choose a Module or enter (e) to exit: ");
-                choice = keyboard.nextLine();
+            } // Out of while validating loop
+            if (choice.equals("1")) {
+                System.out.println("Module 1: ");
+            } else if (choice.equals("2")) {
+                System.out.println("Module 2: ");
+            } else if (choice.equals("3")) {
+                System.out.println("Module 3: ");
+            } else if (choice.equals("4")) {
+                System.out.println("Module 4: ");
             }
         }
     }
