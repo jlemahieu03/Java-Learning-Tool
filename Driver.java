9665ddeb6c0646336bc812e43c11402a105c4140
Zoe <zoe.bell@uvm.edu>
2024-04-13 10:21:58 -0400

    Edited Driver

--- Driver.java
+++ Driver.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Driver{
     public static void main(String [] args){
-        startLesson();
+        learnPython();
     }
-    public static void startLesson(){
-        System.out.println("Welcome to the class! We will be learning Python today")
+    public static void learnPython(){
+        keyboard = new Scanner(System.in);
+        System.out.println("Welcome to the class! We will be learning Python today");
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("Choose a module to start learning: ");
+            System.out.println("(1) for Module 1");
+            System.out.println("(2) for Module 2");
+            System.out.println("(3) for Module 3");
+            System.out.println("(4) for Module 4");
+
+            // Get user choice for the module
+            String choice = keyboard.nextLine();
+        }
     }
 }
