10e2962d3eec8717dc2b41d2d1bebfc292f405df
James LeMahieu <jlemahie@uvm.edu>
2024-04-13 12:05:51 -0400

    Finished quiz.class

--- Quiz.java
+++ Quiz.java
@@ -5,6 +5,7 @@ public class Quiz {
     private ArrayList<Question> quiz;
     private int maxScore;
     private int playerScore;
+    private ArrayList<Flashcards> wrongQuestions;
 
     public Quiz() {
         quiz = new ArrayList<>();
@@ -28,17 +29,17 @@ public class Quiz {
         return playerScore;
     }
 
-    private boolean isCorrectAnswer(char answer, Question ques) {
-        if ((answer == 'A' || answer == 'a') && ques.getAnswerIndex() == 0) {
+    private boolean isCorrectAnswer(String answer, Question ques) {
+        if ((answer == "A" || answer == "a") && ques.getAnswerIndex() == 0) {
             return true;
         }
-        if ((answer == 'B' || answer == 'b') && ques.getAnswerIndex() == 1) {
+        if ((answer == "B" || answer == "b") && ques.getAnswerIndex() == 1) {
             return true;
         }
-        if ((answer == 'C' || answer == 'c') && ques.getAnswerIndex() == 2) {
+        if ((answer == "C" || answer == "c") && ques.getAnswerIndex() == 2) {
             return true;
         }
-        if (((answer == 'D' || answer == 'd')) && ques.getAnswerIndex() == 3) {
+        if (((answer == "D" || answer == "d")) && ques.getAnswerIndex() == 3) {
             return true;
         }
         return false;
@@ -60,14 +61,44 @@ public class Quiz {
         Scanner keyboard = new Scanner(System.in);
         for (Question q:quiz){
             System.out.print(q + "\n");
-            validateAnswer(keyboard)
+            String userAnswer = validateAnswer(keyboard);
+            if(isCorrectAnswer(userAnswer, q)){
+                playerScore++;
+            } else {
+                ArrayList<String> answersCopy = q.getAnswers();
+                wrongQuestions.add(new Flashcards(q.getQuestion(), answersCopy.get(q.getAnswerIndex())));
+            }
         }
+        keyboard.close();
+        System.out.println(displayResults());
     }
 
-    private char validateAnswer(Scanner keyboard){
+    private String validateAnswer(Scanner keyboard){
+        String answer;
         while(true){
             System.out.println("Enter your answer: ");
+            answer = keyboard.nextLine();
+            if (answer.isEmpty()){
+                System.out.println("No input. Please enter in A, B, C, or D");
+            } else if (answer.length() > 1){
+                System.out.println("Answer more than one character. Please enter in A, B, C, or D");
+            } else {
+                if (answer.equals("A") || answer.equals("a")){
+                    break;
+                }
+                if (answer.equals("B") || answer.equals("b")){
+                    break;
+                }
+                if (answer.equals("C") || answer.equals("b")){
+                    break;
+                }
+                if (answer.equals("D") || answer.equals("d")){
+                    break;
+                }
+                System.out.println("Letter inputted not A, B, C, or D.");
+            }
 
         }
+        return answer;
     }
 }
