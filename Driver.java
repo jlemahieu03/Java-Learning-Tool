da152d594bdfbefc2e16e4b636186ad321b1e368
lilyb <lilyabonds@gmail.com>
2024-04-13 09:41:20 -0400

    driver

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+    public static void main(String [] args){
+        startLesson();
+    }
+    public static void startLesson(){
+        System.out.println("Welcome to the class! We will be learning Python today")
+    }
+}
